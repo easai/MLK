f520ed7fcac10a5c43e6864fc4910c97270ad89a
easai <aksarih@gmail.com>
2014-02-22 13:10:51 -0600

    Added a screenshot

--- gen/easai/ihaveadream/R.java
+++ gen/easai/ihaveadream/R.java
@@ -25,12 +25,13 @@ public final class R {
         public static final int mlk=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int mlk=0x7f090002;
-        public static final int play_button=0x7f090001;
+        public static final int action_settings=0x7f090006;
+        public static final int mlk=0x7f090001;
+        public static final int play_button=0x7f090000;
         public static final int scrollView1=0x7f090003;
+        public static final int share_button=0x7f090005;
         public static final int textView1=0x7f090004;
-        public static final int wiki_button=0x7f090000;
+        public static final int wiki_button=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
